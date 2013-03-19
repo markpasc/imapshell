b5fbf420f7f7012e228d07ddaa165901b30f869d
Mark Paschal <markpasc@markpasc.org>
2013-03-18 22:52:10 -0400

    Switch from termtool to argh

--- imapshell.py
+++ imapshell.py
@@ -4,8 +4,9 @@ from getpass import getpass
 import logging
 from pprint import pprint
 
+import argh
+from argh import arg
 from imapclient import IMAPClient
-from termtool import Termtool, argument, subcommand
 
 
 @contextmanager
@@ -17,41 +18,52 @@ def folder(server, foldername, readonly=False):
         server.close_folder()
 
 
-class Imapshell(Termtool):
+@contextmanager
+def connect(host, no_ssl=False):
+    if '@' in host:
+        # Split from the end in case the username is an email address.
+        username, host = host.rsplit('@', 1)
+    else:
+        username = raw_input(host + " login name: ")
 
-    def connect(self, host, use_ssl=True):
-        if '@' in host:
-            # Split from the end in case the username is an email address.
-            username, host = host.rsplit('@', 1)
-        else:
-            username = raw_input(host + " login name: ")
+    if ':' in host:
+        host, port = host.split(':', 1)
+        port = int(port)
+    else:
+        port = None
 
-        if ':' in host:
-            host, port = host.split(':', 1)
-            port = int(port)
-        else:
-            port = None
+    password = getpass(host + " password: ")
 
-        password = getpass(host + " password: ")
+    use_ssl = not no_ssl
+    server = IMAPClient(host, port, ssl=use_ssl)
+    server.normalise_times = False
+    server.login(username, password)
+    yield server
+    server.logout()
 
-        server = IMAPClient(host, port, ssl=use_ssl)
-        server.normalise_times = False
-        server.login(username, password)
-        return server
 
-    @subcommand(help='test that a server is reachable and try to log in')
-    @argument('host', help='hostname of the IMAP server')
-    @argument('--no-ssl', action='store_false', dest='ssl', help='connect without SSL')
-    def login(self, args):
-        server = self.connect(args.host, args.ssl)
+commands = list()
+
+def cmd(fn):
+    commands.append(fn)
+
+
+@cmd
+@arg('host', help='hostname of the IMAP server')
+@arg('--no-ssl', help='connect without SSL')
+def login(host, no_ssl=False):
+    """Test that a server is reachable and try to log in."""
+    with connect(host, no_ssl) as server:
         logging.info("Connected successfully")
         logging.debug("Server capabilities: %r", server.capabilities())
 
-    @subcommand(help='list available mail folders')
-    @argument('host', help='hostname of the IMAP server')
-    @argument('--no-ssl', action='store_false', dest='ssl', help='connect without SSL')
-    def folders(self, args):
-        server = self.connect(args.host, args.ssl)
+
+@cmd
+@arg('host', help='hostname of the IMAP server')
+@arg('--no-ssl', help='connect without SSL')
+def folders(host, no_ssl=False):
+    """List available mail folders."""
+    with connect(host, no_ssl) as server:
         folders = server.list_folders()
 
         table = self.table(['Name', 'Flags', 'Delimiter', 'Messages', 'Unread'])
@@ -60,109 +72,124 @@ class Imapshell(Termtool):
             table.add_row([name, ' '.join(flags), delimiter, status['MESSAGES'], status['UNSEEN']])
         print table
 
-    @subcommand(help='list messages in a folder')
-    @argument('host', help='hostname of the IMAP server')
-    @argument('folder', help='name of the folder to list')
-    @argument('--no-ssl', action='store_false', dest='ssl', help='connect without SSL')
-    def messages(self, args):
-        server = self.connect(args.host, args.ssl)
-        with folder(server, args.folder, readonly=True):
+
+@cmd
+@arg('host', help='hostname of the IMAP server')
+@arg('folder', help='name of the folder to list')
+@arg('--no-ssl', help='connect without SSL')
+def messages(host, folder, no_ssl=False):
+    """List messages in a folder."""
+    with connect(host, no_ssl) as server:
+        with folder(server, folder, readonly=True):
             message_ids = server.search()
             messages = server.fetch(message_ids, ['BODY.PEEK[HEADER]', 'INTERNALDATE', 'FLAGS'])
 
-        table = self.table(['#', 'From', 'Subject', 'Timestamp', 'Flags'])
-        for message in messages.itervalues():
-            headers = Parser().parsestr(message['BODY[HEADER]'])
-            timestamp = message['INTERNALDATE'].isoformat()
-            flags = ' '.join(message['FLAGS'])
-            table.add_row([message['SEQ'], headers['from'], headers['subject'], timestamp, flags])
-        print table
+    table = self.table(['#', 'From', 'Subject', 'Timestamp', 'Flags'])
+    for message in messages.itervalues():
+        headers = Parser().parsestr(message['BODY[HEADER]'])
+        timestamp = message['INTERNALDATE'].isoformat()
+        flags = ' '.join(message['FLAGS'])
+        table.add_row([message['SEQ'], headers['from'], headers['subject'], timestamp, flags])
+    print table
 
-    @subcommand(help='copy messages from one server to another')
-    @argument('left_server', metavar='from', help='server to copy messages from')
-    @argument('left_folder', metavar='from_folder', help='folder on "from" server to copy messages from')
-    @argument('right_server', metavar='to', help='server to copy messages to')
-    @argument('right_folder', metavar='to_folder', help='folder on "to" server to copy messages to')
-    @argument('--from-no-ssl', action='store_false', dest='left_ssl', help='connect to "from" server without SSL')
-    @argument('--to-no-ssl', action='store_false', dest='right_ssl', help='connect to "to" server without SSL')
-    def migrate(self, args):
-        left_server = self.connect(args.left_server, args.left_ssl)
-        with folder(left_server, args.left_folder, readonly=True):
-            message_ids = left_server.search()
-            messages = left_server.fetch(message_ids, ['BODY.PEEK[]', 'INTERNALDATE', 'FLAGS'])
 
-        logging.debug("Found %d messages, copying...", len(messages))
+@cmd
+@arg('from-host', help='server to copy messages from')
+@arg('from-folder', help='folder on "from" server to copy messages from')
+@arg('to-host', help='server to copy messages to')
+@arg('to-folder', help='folder on "to" server to copy messages to')
+@arg('--from-no-ssl', help='connect to "from" server without SSL')
+@arg('--to-no-ssl', help='connect to "to" server without SSL')
+def migrate(from_host, from_folder, to_host, to_folder, from_no_ssl=False, to_no_ssl=False):
+    """Copy messages from one server to another."""
+    with connect(from_host, from_no_ssl) as from_server:
+        with folder(from_server, from_folder, readonly=True):
+            message_ids = from_server.search()
+            messages = from_server.fetch(message_ids, ['BODY.PEEK[]', 'INTERNALDATE', 'FLAGS'])
 
-        right_server = self.connect(args.right_server, args.right_ssl)
+    logging.debug("Found %d messages, copying...", len(messages))
+
+    with connect(to_host, to_no_ssl) as to_server:
         for message in messages.itervalues():
             # \Recent is a "read-only" flag so don't try to set it.
             flags = [flag for flag in message['FLAGS'] if flag.lower() != '\\recent']
-            right_server.append(args.right_folder, message['BODY[]'], flags, message['INTERNALDATE'])
+            to_server.append(to_folder, message['BODY[]'], flags, message['INTERNALDATE'])
             logging.debug("appended message %r", message['SEQ'])
 
-        logging.info("Copied %d messages", len(messages))
+    logging.info("Copied %d messages", len(messages))
 
-    @subcommand(help='peek at a message')
-    @argument('host', help='hostname of the IMAP server')
-    @argument('folder', help='name of the folder containing the message to show')
-    @argument('message', help='message ID of the message to show')
-    @argument('--no-ssl', action='store_false', dest='ssl', help='connect without SSL')
-    def peek(self, args):
-        server = self.connect(args.host, args.ssl)
-        with folder(server, args.folder, readonly=True):
-            messages = server.fetch([args.message], ['BODY.PEEK[]', 'INTERNALDATE', 'FLAGS'])
-            for messageid, message in messages.items():
-                logging.debug("Message %r has keys %s", messageid, ', '.join(message.keys()))
-                body = message['BODY[]']
-                timestamp = message['INTERNALDATE'].isoformat()
-                flags = ' '.join(message['FLAGS'])
 
-                print messageid, timestamp, flags
-                print body
-                print
+@cmd
+@arg('host', help='hostname of the IMAP server')
+@arg('folder', help='name of the folder containing the message to show')
+@arg('messageid', help='message ID of the message to show')
+@arg('--no-ssl', help='connect without SSL')
+def peek(host, folder, messageid, no_ssl=False):
+    """Peek at a message."""
+    with connect(host, no_ssl) as server:
+        with folder(server, folder, readonly=True):
+            messages = server.fetch([messageid], ['BODY.PEEK[]', 'INTERNALDATE', 'FLAGS'])
+    for mid, message in messages.items():
+        logging.debug("Message %r has keys %s", mid, ', '.join(message.keys()))
+        body = message['BODY[]']
+        timestamp = message['INTERNALDATE'].isoformat()
+        flags = ' '.join(message['FLAGS'])
 
-    @subcommand(help='merge a folder into another on one server')
-    @argument('host', help='hostname of the IMAP server')
-    @argument('left_folder', metavar='from_folder', help='folder to move messages from')
-    @argument('right_folder', metavar='to_folder', help='folder to move messages into')
-    @argument('--no-ssl', action='store_false', dest='ssl', help='connect without SSL')
-    def merge(self, args):
-        server = self.connect(args.host, args.ssl)
+        print mid, timestamp, flags
+        print body
+        print
 
-        with folder(server, args.left_folder):  # read-write
+
+@cmd
+@arg('host', help='hostname of the IMAP server')
+@arg('from-folder', help='folder to move messages from')
+@arg('to-folder', help='folder to move messages to')
+@arg('--no-ssl', help='connect without SSL')
+def merge(host, from_folder, to_folder, no_ssl=False):
+    """Merge a folder into another on one server.
+
+    The folder from which messages are moved is left empty.
+
+    """
+    with connect(host, no_ssl) as server:
+        with folder(server, from_folder):  # read-write
             message_ids = server.search()
-            result = server.copy(message_ids, args.right_folder)
+            result = server.copy(message_ids, to_folder)
             logging.debug("Moved %d messages", len(message_ids))
             server.delete_messages(message_ids)
             server.expunge()
 
-        logging.info("Moved %d messages from %s to %s", len(message_ids), args.left_folder, args.right_folder)
+    logging.info("Moved %d messages from %s to %s", len(message_ids), from_folder, to_folder)
 
-    @subcommand(help='create a new mail folder')
-    @argument('host', help='hostname of the IMAP server')
-    @argument('folder', help='name of the folder to create')
-    @argument('--no-ssl', action='store_false', dest='ssl', help='connect without SSL')
-    def createfolder(self, args):
-        server = self.connect(args.host, args.ssl)
-        server.create_folder(args.folder)
-        logging.info("Created!")
 
-    @subcommand(help='delete a mail folder')
-    @argument('host', help='hostname of the IMAP server')
-    @argument('folder', help='name of the folder to delete')
-    @argument('--yes', action='store_true', help='delete the folder even if it contains messages')
-    @argument('--no-ssl', action='store_false', dest='ssl', help='connect without SSL')
-    def rmfolder(self, args):
-        server = self.connect(args.host, args.ssl)
+@cmd
+@arg('host', help='hostname of the IMAP server')
+@arg('folder', help='name of the folder to create')
+@arg('--no-ssl', help='connect without SSL')
+def createfolder(host, folder, no_ssl=False):
+    """Create a new mail folder."""
+    with self.connect(host, no_ssl) as server:
+        server.create_folder(folder)
+    logging.info("Created!")
 
-        status = server.folder_status(args.folder, ['MESSAGES'])
-        if status['MESSAGES'] > 0 and not args.yes:
-            logging.error("Folder %s contains messages", args.folder)
+
+@cmd
+@arg('host', help='hostname of the IMAP server')
+@arg('folder', help='name of the folder to delete')
+@arg('--yes', help='delete the folder even if it contains messages')
+@arg('--no-ssl', help='connect without SSL')
+def rmfolder(host, folder, yes=False, no_ssl=False):
+    """Delete a mail folder."""
+    with connect(host, no_ssl) as server:
+        status = server.folder_status(folder, ['MESSAGES'])
+        if status['MESSAGES'] > 0 and not yes:
+            logging.error("Folder %s contains messages", folder)
             return
 
-        server.delete_folder(args.folder)
-        logging.info("Deleted %s with %d messages", args.folder, status['MESSAGES'])
+        server.delete_folder(folder)
+
+    logging.info("Deleted %s with %d messages", folder, status['MESSAGES'])
 
 
 if __name__ == '__main__':
-    Imapshell().run()
+    argh.dispatch_commands(commands)
